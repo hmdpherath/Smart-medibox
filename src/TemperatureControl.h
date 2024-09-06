6be50932a30499a8268de01b1a4995f69ee2e1ec
Dasuni Herath <164705433+hmdpherath@users.noreply.github.com>
2024-09-06 12:31:22 +0530

    Create TemperatureControl.h

---
+++ src/TemperatureControl.h
@@ -0,0 +1,27 @@
+
+// TemperatureControl.h
+#ifndef TEMPERATURE_CONTROL_H
+#define TEMPERATURE_CONTROL_H
+
+#include <DHTesp.h>
+#include "OLEDControl.h"
+
+class TemperatureControl {
+public:
+    #define LED_2 2
+  
+  TemperatureControl(int dhtPin, OLEDControl &oled);
+  void setup();
+  bool checkTemperature();
+
+private:
+  DHTesp dhtSensor;
+  OLEDControl &oledControl;  // Reference to the OLEDControl instance
+  int minTemp;
+  int maxTemp;
+  int minHumidity;
+  int maxHumidity;
+  int ledPin;
+};
+
+#endif
