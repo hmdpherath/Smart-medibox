bf4a2431424470543ec0cfc74c8028df1435122d
Dasuni Herath <164705433+hmdpherath@users.noreply.github.com>
2024-09-06 12:26:48 +0530

    Create BuzzerControl.h

---
+++ src/BuzzerControl.h
@@ -0,0 +1,14 @@
+// BuzzerControl.h
+#ifndef BUZZERCONTROL_H
+#define BUZZERCONTROL_H
+
+// Function to initialize the buzzer
+void setupBuzzer(int buzzerPin);
+
+// Function to activate the buzzer
+void activateBuzzer();
+
+// Function to deactivate the buzzer
+void deactivateBuzzer();
+
+#endif
